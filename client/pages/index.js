60471d5f39271d41d827ce61ba7a824218884d55
Reza Norouzzadeh <reza.nrzdh@gmail.com>
2022-06-05 13:11:00 +0430

    client index page

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+const IndexPage = () => {
+    return (
+        <>
+            <h1>Home Page</h1>
+            <p>lorem ipsum dolor esmit</p>       
+        </>
+
+    );
+}
+
+export default IndexPage;
