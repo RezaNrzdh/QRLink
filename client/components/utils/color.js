3bd373642cd9c015a7c6479824183a0a764bc33a
RezaNrzdh <reza.nrzdh@gmail.com>
2022-06-05 22:59:46 +0430

    color & typography

---
+++ client/components/utils/color.js
@@ -0,0 +1,57 @@
+const Color = {
+    primary: {
+        main: '',
+        dark: '',
+        light: ''
+    },
+    secondary: {
+        main: '',
+        dark: '',
+        light: ''
+    },
+    text: {
+        main: '#23425f',
+        light: '#767676',
+        contrast: '#ffffff'
+    },
+    background: {
+        main: '#F5F5F5',
+        dark: '#EBEBEB',
+        light: '#ffffff'
+    },
+    stroke: {
+        main: '#DDDDDD',
+        dark: '#CCCCCC',
+        light: '#EAEAEA'
+    },
+    action: {
+        outlined: '#23425f'
+    },
+    success: {
+        main: '#35D0BA',
+        dark: '#18A4AD',
+        light: '#88EDD5'
+    },
+    danger: {
+        main: '#EA4961',
+        dark: '#AA2337',
+        light: '#FAA8B5'
+    },
+    warning: {
+        main: '',
+        dark: '',
+        light: ''
+    },
+    info: {
+        main: '',
+        dark: '',
+        light: ''
+    },
+    icon: {
+        main: '',
+        dark: '',
+        light: ''
+    }
+}
+
+export {Color};
---
+++ client/components/utils/typography.js
@@ -0,0 +1,53 @@
+import styled, {css} from 'styled-components';
+
+export const Headline1 = css`
+    font-size: 2.5rem;
+    font-weight: 700;
+`;
+
+export const Headline2 = css`
+    font-size: 2rem;
+    font-weight: 700;
+`;
+
+export const Headline3 = css`
+    font-size: 1.75rem;
+    font-weight: 700;
+`;
+
+export const Headline4 = css`
+    font-size: 1.5rem;
+    font-weight: 700;
+`;
+
+export const Headline5 = css`
+    font-size: 1.25rem;
+    font-weight: 700;
+`;
+
+export const Headline6 = css`
+    font-size: 1.125rem;
+    font-weight: 700;
+`;
+
+export const Subtitle1 = css`
+    font-size: 1rem;
+    font-weight: 700;
+`;
+
+export const Subtitle2 = css`
+    font-size: 0.875rem;
+    font-weight: 700;
+`;
+
+export const Body1 = css`
+    font-size: 1rem;
+`;
+
+export const Body2 = css`
+    font-size: 0.875rem;
+`;
+
+export const Caption = css`
+    font-size: 0.75rem;
+`;
