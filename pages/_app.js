15c8b9a4240dccdfde968febc65f6867e85945ac
RezaNrzdh <reza.nrzdh@gmail.com>
2022-06-20 00:22:42 +0430

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import {GlobalStyle} from 'components/globalStyled';
 import MainProvider from 'context/mainContext';
 
 function App({Component, pageProps}) {
+    console.log('App');
     return (
         <MainProvider>
             <GlobalStyle />
