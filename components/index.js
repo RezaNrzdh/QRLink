8d4b70de9bdfb9f65b4b7510472675df9fa5090c
RezaNrzdh <reza.nrzdh@gmail.com>
2022-07-04 12:20:35 +0430

    update

--- components/index.js
+++ components/index.js
@@ -6,10 +6,10 @@ export * from 'components/common/textButton';
 export * from 'components/common/textfield';
 export * from 'components/common/layout';
 export * from 'components/common/authLayout';
+export * from 'components/common/navigation';
 
 // Desktop
 export * from 'components/header';
 export * from 'components/footer';
-export * from 'components/navigation/desktop';
 
 //Mobile
