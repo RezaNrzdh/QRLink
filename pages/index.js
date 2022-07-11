da6f79f6433f45a09d1563b2c8f282737db3e31e
RezaNrzdh <reza.nrzdh@gmail.com>
2022-07-11 16:21:08 +0430

    update

--- pages/index.js
+++ pages/index.js
@@ -6,45 +6,20 @@ import axios from 'axios';
 const Desktop = dynamic(() => import('components/view/home/desktop'));
 const Mobile  = dynamic(() => import('components/view/home/mobile'));
 
-const customers = [
-    {
-        logo: '/images/neshan.svg',
-        desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز',
-        author: 'محمد زاهدی - بنیانگذار نشان'
-    },
-    {
-        logo: '/images/zarinpal.svg',
-        desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز',
-        author: 'محمد زاهدی - بنیانگذار نشان'
-    },
-    {
-        logo: '/images/namava.svg',
-        desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز',
-        author: 'محمد زاهدی - بنیانگذار نشان'
-    },
-    {
-        logo: '/images/lahzenegar.svg',
-        desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز',
-        author: 'محمد زاهدی - بنیانگذار نشان'
-    }
-];
-
-const IndexPage = ({s}) => {
-    console.log(s);
+const IndexPage = ({customers}) => {
     const {isMobile} = useContext(mainContext);
 
     return isMobile === true
         ? <Mobile />
-        : <Desktop customers={customers} />
+        : <Desktop customers={customers.data} />
 }
 
-export const getInitialProps  = async (context) => {
-
-    const dd = await axios.get('/api/customer');
-
+export const getStaticProps  = async (context) => {
+    const {data} = await axios.get(`${process.env.HOSTNAME}/api/customer`);
+    
     return {
         props: {
-            s: dd
+            customers: data
         }
     }
 }
